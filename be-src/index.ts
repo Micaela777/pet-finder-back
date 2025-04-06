ba542e8461c6010cb15d05e5fde785f4b685c222
Micaela Medina <micaelamedina@MacBook-Air-de-Micaela.local>
2025-04-06 17:47:39 -0300

    modified url

--- be-src/index.ts
+++ be-src/index.ts
@@ -24,7 +24,7 @@ const app = express()
 const port = process.env.PORT || 3003
 
 app.use(cors({
-  origin: "https://dwf-m8-final.web.app/",
+  origin: "https://dwf-m8-final.web.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }))
